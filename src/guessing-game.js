20915e630c287bcf6e6fe4869aa29a8081ede295
Kirill Litvin <litvin.kirill.m@mail.ru>
2021-09-06 21:36:52 +0300

    feat: add result

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.result = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.result = Math.ceil((this.max - this.min) / 2 + this.min);
     }
 
     guess() {
-
+        return this.result;
     }
 
     lower() {
-
+        this.max = this.result;
+        this.result = Math.ceil((this.max - this.min) / 2 + this.min);
     }
 
     greater() {
-
+        this.min = this.result;
+        this.result = Math.ceil((this.max - this.min) / 2 + this.min);
     }
 }
 
